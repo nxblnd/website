616d726b65f361c9dd3c108b815f23e5d404d9de
Pavel Egorov <contact@pavelegorov.xyz>
2025-07-12 02:42:46 +0300

    Add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+    integrations: [icon()],
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.fontsource(),
+                name: 'Alegreya',
+                cssVariable: '--font-alegreya',
+            },
+            {
+                provider: fontProviders.fontsource(),
+                name: 'Alegreya Sans',
+                cssVariable: '--font-alegreya-sans',
+            },
+            {
+                provider: fontProviders.fontsource(),
+                name: 'JetBrains Mono',
+                cssVariable: '--font-jetbrains-mono',
+            },
+        ],
+    },
 });
