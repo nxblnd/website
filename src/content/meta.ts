be07e421c6a73ec058aee6d6fed9272083648d65
Pavel Egorov <contact@pavelegorov.xyz>
2025-08-08 22:26:14 +0300

    Create navbar from meta.menu object

--- src/content/meta.ts
+++ src/content/meta.ts
@@ -1,8 +1,30 @@
+interface MenuItem {
+    name: string,
+    value: string,
+    path: string,
+    icon?: string,
+}
+
 export interface Meta {
     title: string,
+    menu: Array<MenuItem>,
 }
 
 export const meta: Meta = {
     title: 'title?',
+    menu: [
+        {
+            name: 'cv',
+            value: 'Резюме',
+            path: 'cv',
+            icon: 'mdi:resume',
+        },
+        {
+            name: 'about',
+            value: 'About',
+            path: 'about',
+            icon: 'mdi:about',
+        },
+    ],
 }
 
