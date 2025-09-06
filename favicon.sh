fa1c735c3467ec76805dd9153501c5414dfc17f7
Pavel Egorov <contact@pavelegorov.xyz>
2025-09-06 09:23:31 +0300

    Add favicon

---
+++ favicon.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+cd public/ || exit
+mkdir -p favicon
+
+size_list="16 32 48 64 76 96 120 128 144 152 180 196"
+
+for size in ${size_list}
+do
+    rsvg-convert --width="$size" --height="$size" --keep-aspect-ratio favicon.svg > "favicon/$size.png"
+done
+
+echo 'PNG icons before compression' "$(du -b favicon/)"
+
+pngcrush -ow favicon/*.png
+
+echo 'PNG icons after compression' "$(du -b favicon/)"
+
+cp favicon/180.png ./apple-touch-icon.png
+
+magick favicon/16.png favicon/32.png favicon/48.png favicon/64.png -colors 256 favicon.ico
