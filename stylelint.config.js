750457d220484c24dd86142bfd74a4f97f68aaa8
Pavel Egorov <contact@pavelegorov.xyz>
2025-09-10 22:44:00 +0300

    Add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,8 @@
+export default {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-html",
+        "stylelint-config-idiomatic-order",
+    ],
+    plugins: ["stylelint-order"],
+};
