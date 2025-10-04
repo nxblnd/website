20f438599c01d4b699dac588f06373e0c0d28b02
Pavel Egorov <contact@pavelegorov.xyz>
2025-10-04 09:23:21 +0300

    Fix debounce function

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,14 +1,10 @@
 export function debounce(fn: (...args: Array<unknown>) => unknown, wait: number) {
     let timeoutId: number | undefined;
-    return (...args: Array<unknown>) => {
-        if (!timeoutId) {
-            fn(...args);
-        }
-
+    return (...args: Parameters<typeof fn>) => {
         window.clearTimeout(timeoutId);
 
         timeoutId = window.setTimeout(() => {
-            timeoutId = undefined;
+            fn(...args);
         }, wait);
     };
 }
